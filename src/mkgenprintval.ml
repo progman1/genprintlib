3f0299fa299edef23782a43dd12e72bb4e021504
progman1 <dev@example.com>
2019-07-19 15:45:08 +0100

    account for all ocaml version changes...

--- src/mkgenprintval.ml
+++ src/mkgenprintval.ml
@@ -1,9 +1,29 @@
 let _=
   let v=Sys.ocaml_version in
+  if v < "4.03.0" then(
+    Unix.rename "genprintval.ml-2" "genprintval.ml";
+    Unix.rename "genprintval.mli-2" "genprintval.mli"
+  )else
+  if v < "4.04.0" then(
+    Unix.rename "genprintval.ml-3" "genprintval.ml";
+    Unix.rename "genprintval.mli-3" "genprintval.mli"
+  )else
+  if v < "4.05.0" then(
+    Unix.rename "genprintval.ml-4" "genprintval.ml";
+    Unix.rename "genprintval.mli-4" "genprintval.mli"
+  )else
+  if v < "4.06.0" then(
+    Unix.rename "genprintval.ml-5" "genprintval.ml";
+    Unix.rename "genprintval.mli-5" "genprintval.mli"
+  )else
+  if v < "4.07.0" then(
+    Unix.rename "genprintval.ml-6" "genprintval.ml";
+    Unix.rename "genprintval.mli-6" "genprintval.mli"
+  )else
   if v < "4.08.0" then(
-    Unix.rename "genprintval.ml-epoc1" "genprintval.ml";
-    Unix.rename "genprintval.mli-epoc1" "genprintval.mli"
+    Unix.rename "genprintval.ml-7" "genprintval.ml";
+    Unix.rename "genprintval.mli-7" "genprintval.mli"
   )else(
-    Unix.rename "genprintval.ml-epoc2" "genprintval.ml";
-    Unix.rename "genprintval.mli-epoc2" "genprintval.mli"
+    Unix.rename "genprintval.ml-8" "genprintval.ml";
+    Unix.rename "genprintval.mli-8" "genprintval.mli"
   )

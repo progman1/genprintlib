d1c770e7b5c2f2731a92d79bc3d6cdbf8fef6d73
progman1 <dev@example.com>
2019-07-19 14:40:54 +0100

    a generator added as per recommendation/contributed code

---
+++ src/mkgenprintval.ml
@@ -0,0 +1,9 @@
+let _=
+  let v=Sys.ocaml_version in
+  if v < "4.08.0" then(
+    Unix.rename "genprintval.ml-epoc1" "genprintval.ml"
+    Unix.rename "genprintval.mli-epoc1" "genprintval.mli"
+  )else(
+    Unix.rename "genprintval.ml-epoc2" "genprintval.ml"
+    Unix.rename "genprintval.mli-epoc2" "genprintval.mli"
+  )
